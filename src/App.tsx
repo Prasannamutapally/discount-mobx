2102999d983b72b3067f26e346a4d82f9ce3608b
Prasanna Mutapally <prasannamutapally@gmail.com>
2024-05-08 12:55:30 +0530

    initial app

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,11 @@ import React, { useEffect } from "react";
 import { observer } from "mobx-react-lite";
 import { RowData, store } from "./DiscountStore";
 import { action } from "mobx";
+import { IoIosInformationCircleOutline } from "react-icons/io";
+import { RiAddLine } from "react-icons/ri";
+import { AiOutlineClose } from "react-icons/ai";
+import { FaDollarSign } from "react-icons/fa";
+import "./App.css";
 
 const App: React.FC = observer(() => {
   useEffect(() => {
@@ -48,25 +53,99 @@ const App: React.FC = observer(() => {
 
   return (
     <div className="discount-calculator">
-      <h1>Discount calculator</h1>
-      <div className="container">
+      <h1
+        style={{ color: "white", fontFamily: "roboto", marginBottom: "100px" }}
+      >
+        Discount calculator
+      </h1>
+      <div className="pricing-container">
         <div className="input-container">
-          <label>Pricing option:</label>
-          <select value={store.priceOption} onChange={handleChangePriceOption}>
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              width: "50%",
+            }}
+          >
+            <label style={{ marginRight: "5px", color: "white" }}>
+              Select Pricing option:
+            </label>
+            <IoIosInformationCircleOutline style={{ color: "white" }} />
+          </div>
+          <select
+            value={store.priceOption}
+            onChange={handleChangePriceOption}
+            style={{
+              width: "50%",
+              height: "60%",
+              border: "none",
+              outline: "none",
+            }}
+            className="dropdown"
+          >
             <option value="volume">Volume</option>
             <option value="each">Each Unit</option>
           </select>
         </div>
         <div className="input-container">
-          <label>Price (USD):</label>
-          <input
-            type="text"
-            value={store.price.toString()} // Convert price to string for input value
-            onChange={handleChangePrice}
-          />
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              width: "50%",
+            }}
+          >
+            <label style={{ marginRight: "5px", color: "white" }}>
+              Enter Price (USD)
+            </label>
+            <IoIosInformationCircleOutline style={{ color: "white" }} />
+          </div>
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              width: "50%",
+              height: "60%",
+              border: "1px solid #ccc",
+            }}
+          >
+            <FaDollarSign
+              style={{ color: "white", height: "15px", marginLeft: "5px" }}
+            />
+            <input
+              type="text"
+              value={store.price.toString()}
+              onChange={handleChangePrice}
+              style={{
+                border: "none",
+                flex: 1,
+                marginLeft: "5px",
+                outline: "none",
+                height: "100%",
+              }}
+            />
+          </div>
         </div>
       </div>
-      <div className="container">
+      <div style={{ display: "flex", flexDirection: "column" }}>
+        <button
+          onClick={handleAddRow}
+          style={{
+            color: "rgba(28, 40, 84, 1)",
+            display: "flex",
+            alignItems: "center",
+            padding: "6px 14px",
+            backgroundColor: "white",
+            borderRadius: "5px",
+            alignSelf: "flex-end",
+            fontWeight: "600",
+            fontFamily: "roboto",
+            fontSize: "15px",
+            marginBottom: "10px",
+          }}
+        >
+          <RiAddLine style={{ marginRight: "5px" }} /> Add row
+        </button>
         <table>
           <thead>
             <tr>
@@ -88,6 +167,7 @@ const App: React.FC = observer(() => {
                     type="text"
                     value={row.last}
                     onChange={(e) => handleInputChange(index, "last", e)}
+                    style={{ outline: "none" }}
                   />
                 </td>
                 <td>{row.units}</td>
@@ -98,14 +178,33 @@ const App: React.FC = observer(() => {
                     onChange={(e) =>
                       handleInputChange(index, "discountPercentage", e)
                     }
+                    style={{ outline: "none" }}
                   />
                 </td>
-                <td>{row.originalPrice}</td>
-                <td>{row.discountedPrice}</td>
+                <td
+                  style={{
+                    color:
+                      row.originalPrice > row.discountedPrice ? "red" : "white",
+                    fontWeight: "500",
+                  }}
+                >
+                  {row.originalPrice}
+                </td>
+                <td
+                  style={{
+                    color:
+                      row.discountedPrice < row.originalPrice
+                        ? "green"
+                        : "white",
+                    fontWeight: "500",
+                  }}
+                >
+                  {row.discountedPrice}
+                </td>
                 <td>
                   {index > 0 && (
                     <button onClick={() => handleDeleteRow(index)}>
-                      <i className="fas fa-times"></i>
+                      <AiOutlineClose style={{ color: "white" }} />
                     </button>
                   )}
                 </td>
@@ -113,7 +212,6 @@ const App: React.FC = observer(() => {
             ))}
           </tbody>
         </table>
-        <button onClick={handleAddRow}>Add row</button>
       </div>
     </div>
   );
